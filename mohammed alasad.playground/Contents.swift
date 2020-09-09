0fd33310faf1aa9c2f624c7a54d5c1a09cb0df5c
Buasad <hamoodat@live.com>
2020-09-09 20:42:54 +0300

    done

---
+++ mohammed alasad.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+let name = "Mohammed_Alasad"
+var age = 17
+var height = 170
+var hobby1 = "exploring"
+var hobby2 = "trying_out_new_coffee_shops"
+print("my name is \(name) and my age is \(age) and my hobbies are \(hobby1) and \(hobby2)")
+var myFriendName = "ali"
+var friendAge = 13
+var friendHeight = 170
+var friendHobby1 = "exploring"
+var friendHobby2 = "sleeping"
+print(age-friendAge)
+var mutualhobby = (hobby1 == friendHobby1) || (hobby2 == friendHobby1) || (hobby1 == friendHobby2) || (hobby2 == friendHobby2)
+print (mutualhobby)
+print (friendHeight-height)
+
